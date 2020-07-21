d5322a9790c04ea5402ed04d361b15cade52e649
sunmengjie <sunmengjie@meituan.com>
2020-07-21 17:43:59 +0800

    add 2 line

--- src/main/java/com/Hello.java
+++ src/main/java/com/Hello.java
@@ -3,5 +3,6 @@ package com;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Git1 !!!");
+        System.out.println("Hello, Git2 !!!");
     }
 }
